958eff547b7579d3d645a4dcd7ecbd203ad807fe
Jack Brewer <h[afiwhd>
2020-01-27 16:39:19 +0000

    S1C1 - Card class
    The initial card class has been established with a test example.

--- CCG Easy Builder (Unity Project)/Assets/Scripts/Card.cs
+++ CCG Easy Builder (Unity Project)/Assets/Scripts/Card.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-
 public class Card : ScriptableObject
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Fields should include health, attack, description and name.
+    [SerializeField, Header("Card Info")]
+    private string _cardName;
+    [SerializeField]
+    private string _description;
+    [SerializeField]
+    private int _health;
+    [SerializeField]
+    private int _attack;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string CardName { get => _cardName; set => _cardName = value; }
+    public string Description { get => _description; set => _description = value; }
+    public int Health { get => _health; set => _health = value; }
+    public int Attack { get => _attack; set => _attack = value; }
 }
